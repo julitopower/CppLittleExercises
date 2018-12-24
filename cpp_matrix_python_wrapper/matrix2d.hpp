f8ff9f07ffd9b6cb990daa5c9db806a2f15c249b
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2018-12-24 16:20:14 +0000

    ISSUE-N/A Matrix2d: Add doxygen comments and cleanup CMake

--- cpp_matrix_python_wrapper/matrix2d.hpp
+++ cpp_matrix_python_wrapper/matrix2d.hpp
@@ -33,40 +33,41 @@ namespace matrix {
     T* data_end_;
   };
 
-  /*
-   * A two dimensional matrix view of a buffer. This class doesn't own the buffer
+  /*!
+   * \brief A two dimensional matrix view of a buffer. This class doesn't own the buffer
    */
   template<typename T>
   class Matrix2d {
   public:
-    /*
-     * Allows iterating through the different rows in the array
+    /*!
+     * \brief Allows iterating through the different rows in the array
      */
     class VectorViewIterator {
     public:
-      /*
-       * Construct a VectorViewIterator
+      /*!
+       * \brief Construct a VectorViewIterator
        * 
-       * data: The buffer
-       * size: the number of elements of type T in the buffer
+       * \param data The buffer
+       * \param size the number of elements of type T in the buffer
        */
       VectorViewIterator(T* data, std::size_t size) : data_{data}, size_{size} {}
 
-      /*
-       * This method "extracts"" the contents from the iterator.
+      /*!
+       * \brief This method "extracts"" the contents from the iterator.
        * 
-       * return a new VectorView wrapping the portion of the buffer
+       * \return a new VectorView wrapping the portion of the buffer
        * correspoding to a vector of size_ elements of type T
        */
       VectorView<T> get() const { return VectorView<T>(data_, size_); }
 
-      /*
-       * Updates the iterator to point to the next vector
+      /*!
+       * \brief Update the iterator to point to the next vector
        */
       void operator++() { data_ += size_; }
 
-      /*
-       * Comparison is based on the address of the buffer alone.
+      /*!
+       * \brief Comparison is based on the address of the buffer alone.
+       * 
        * We may need to include also the size, if we ever use different
        * views into the same buffer
        */
@@ -74,8 +75,8 @@ namespace matrix {
 	return data_ == other.data_;
       }
 
-      /*
-       * Counterpart of ==
+      /*!
+       * \brief Counterpart of ==
        */
       bool operator!=(const VectorViewIterator& other) const {
 	return data_ != other.data_;
@@ -88,40 +89,48 @@ namespace matrix {
     // For convenience, the type of the elements contained in the matrix
     typedef T Type;
 
-    /* 
-     * Constructs a matrix out of a buffer and dimenstions
+    /*!
+     * \brief Constructs a matrix out of a buffer and dimenstions
      *
-     * buffer, the data buffer
-     * rows, the number of rows in the matrix
-     * cols, the number of columns in the matrix*/
+     * \param buffer the data buffer
+     * \param rows the number of rows in the matrix
+     * \param cols the number of columns in the matrix
+     */
     Matrix2d(T* buffer, std::int64_t rows, std::int64_t cols) :
       data_{buffer}, cols_{cols}, rows_{rows}{}
 
-    /*
-     * The matrix doesn't own the buffer, so here we make sure
+    /*!
+     * \brief The matrix doesn't own the buffer, so here we make sure
      * the destructor does nothing.
      */
     ~Matrix2d() {}
 
-    /*
-     * Get an iterator pointing to the first row in the matrix
+    /*!
+     * \brief Get an iterator pointing to the first row in the matrix
      */
     VectorViewIterator begin() {
       return VectorViewIterator(data_, cols_);
     }
 
+    /*!
+     * \brief Get a const iterator pointing to the first row in the matrix
+     */      
     const VectorViewIterator begin() const {
       return VectorViewIterator(data_, cols_);
     }    
 
-    /*
-     * Get an iterator pointing 1 past the last element in the 
+    /*!
+     * \brief Get an iterator pointing 1 past the last element in the 
      * last vector of the matrix
      */
     VectorViewIterator end() {
       return VectorViewIterator(data_ + rows_ * cols_, cols_);
     }
 
+    /*!
+     * \brief Get a const iterator pointing 1 past the last element in the 
+     * last vector of the matrix
+     */      
     const VectorViewIterator end() const {
       return VectorViewIterator(data_ + rows_ * cols_, cols_);
     }    
