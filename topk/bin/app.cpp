b352882d43e14175639a22a53c8731c7c323e109
julitopower <julio.delgadomangas@gmail.com>
2020-11-29 08:28:32 -0800

    Add topk buffer optimzed implementation

--- topk/bin/app.cpp
+++ topk/bin/app.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <chrono>
 
 #include "topk.hpp"
+
+#define T_START { \
+  auto t0 = std::chrono::high_resolution_clock::now();
+
+#define T_END \
+  auto t1 = std::chrono::high_resolution_clock::now(); \
+  auto span = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count(); \
+  std::cout << "Time [ms]: " << span << std::endl; \
+}
+
+namespace {
+std::vector<int> generate_test_data(std::size_t n, std::size_t seed = 42) {
+  std::srand(seed);
+  std::vector<int> data(n, 0);
+  for (auto i = 0U ; i < n ; ++i) {
+    data[i] = std::rand();
+  }
+  return data;
+}
+}
 
 int main(int argc, char** argv) {
   std::cout << "Hi there" << std::endl;
 
-  std::vector<int> v{3, 2, 5, 4, 1};
+  const auto size = 10000000;
+  std::vector<int> v = generate_test_data(size, 42);
   auto fn = [](const int& i) {
-              return (i < 3) ? (i + 2) : -i;
+              return -i;
             };
-  
+
+
   auto t = topk::buildTopK(v.begin(), v.end(), fn);
 
-  for (auto& v : t.topk(5)) {
-    std::cout << v << std::endl;
+  T_START;
+  t.topkmem(10);
+  T_END;
+    
+  for (auto k = 1 ; k < 30 ; ++k) {
+    std::cout << "k: " << k << " - ";
+    T_START;
+    t.topk(k);
+    T_END;
   }
   
 }
--- topk/include/topk.hpp
+++ topk/include/topk.hpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <type_traits>
+#include <cmath>
 
 namespace topk {
 
@@ -15,29 +16,84 @@ private:
   Fn fn_;
   
 public:
-  TopK(Iter begin, Iter end, Fn fn) : begin_{begin}, end_{end}, fn_{fn} {
+  TopK(Iter begin, Iter end, Fn fn) : begin_{begin}, end_{end}, fn_{fn} {}
+
+  /**
+   * Return the Top K elements based on the evaluation of a cost function on
+   * each element of the input vector.
+   *
+   * This implementation selects the fastest implementation based on computational
+   * complexity calculations.
+   */
+  std::vector<T> topk(std::size_t k) {
+    const auto n = (end_ - begin_);
+    // Requested more elements that available. Use the solution that loads
+    // all the dataset at once.
+    if (k >= n) {
+      return topkmem(n);
+    }
+
+    // Select implementation based on computational complexity.
+    if (k < std::log2l(n)) {
+      return topkbuffer(k);
+    } else {
+      return topkmem(k);
+    }
     
   }
 
   /**
-   * Return the Top K elements based on the evaluation of fn_ on
-   * each element of the input vector.
+   * Return the Top K elements based on the evaluation of a cost function on
+   * each element of the input vector. Algorithm:
    *
-   * - Build buffer vector<sort_value, value> and sort it
-   * - Copy value from pair vector and return it
+   * Use a buffer of k elements to get the topk. This method is faster
+   * than ordering the entire dataset in memory when K < log2(n).
+   *
+   * Use topk instead of this method. Topk automatically selects the fastest
+   * implementation based on their computational complexity.
    */
-  std::vector<T> topk(std::size_t k) {
+  std::vector<T> topkbuffer(std::size_t k) {
     // Buffer vector = vector<pair<sort_value, value>>
     std::vector<std::pair<decltype(fn_(*begin_)), T>> buffer{};
-    buffer.reserve(k);
-    buffer.resize(k);
+    std::transform(begin_, begin_ + k + 1, std::back_inserter(buffer),
+                   [this](const T& elem) {
+                     // Evaluate each element and build pair<sort_value, value>
+                     return std::make_pair(fn_(elem), elem);
+                   });
+    
+    std::sort(buffer.begin(), buffer.end());
+    for (auto it = begin_ + k + 1 ; it != end_; ++it) {
+      buffer.back() = std::make_pair(fn_(*it), *it);
+      std::sort(buffer.begin(), buffer.end());      
+    }
 
     // Output vector is simply a vector<T> with k elmenets
     std::vector<T> output{};
-    output.reserve(k);
-    output.resize(k);    
+    // Copy the topk values in the output vector
+    std::transform(buffer.begin(), buffer.end() - 1, std::back_inserter(output),
+                   [](decltype(*buffer.begin()) elem) {
+                     return elem.second;
+                   });
+    return output;
+  }
+  
+  /**
+   * Return the Top K elements based on the evaluation of a cost function on
+   * each element of the input vector. Algorithm:
+   *
+   * - Build buffer vector<sort_value, value> and sort it
+   * - Copy value from pair vector and return it
+   *
+   * Use topk instead of this method. Topk automatically selects the fastest
+   * implementation based on their computational complexity.   
+   */
+  std::vector<T> topkmem(std::size_t k) {
+    // Buffer vector = vector<pair<sort_value, value>>
+    std::vector<std::pair<decltype(fn_(*begin_)), T>> buffer{};
 
-    // TODO: Very inefficient code for large input vectors
+    // Output vector is simply a vector<T> with k elements
+    std::vector<T> output{};
+    
     const std::size_t input_size = (end_ - begin_);
     // Pickup up to input_size elements
     k = std::min(input_size, k);
@@ -45,7 +101,7 @@ public:
     // Resize buffer to the size of the input. We are going to
     // evaluate all elements in the input and store the pairs<sort_value, value> here
     buffer.resize(input_size);
-    std::transform(begin_, end_, buffer.begin(),
+    std::transform(begin_, end_, std::back_inserter(buffer),
                    [this](const T& elem) {
                      // Evaluate each element and build pair<sort_value, value>
                      return std::make_pair(fn_(elem), elem);
@@ -55,7 +111,7 @@ public:
     buffer.resize(k);
 
     // Copy the topk values in the output vector
-    std::transform(buffer.begin(), buffer.end(), output.begin(),
+    std::transform(buffer.begin(), buffer.end(), std::back_inserter(output),
                    [](decltype(*buffer.begin()) elem) {
                      return elem.second;
                    });
@@ -64,6 +120,15 @@ public:
 
 };
 
+/*! \brief Build a topk object
+ *  Utility function to avoid having to define the template parameters
+ *  to instantiate a TopK class object.
+ *
+ *  \tparam Iter an iterator that must be dereferenciable and must work with the
+ *           algorithms in the <algorithm> header.
+ *  \tparam Fn A function with signature K(*fn)(T), where T is the type returned
+ *          by dereferencing Iter, and K is any type that supports < comparison
+ */
 template <typename Iter, typename Fn>
 TopK<Iter, Fn> buildTopK(Iter begin, Iter end, Fn fn) {
   return TopK<Iter, Fn>(begin, end, fn);
