fcceb3c6e065eb1cc7428084f19fd233fa47466a
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2020-12-08 15:34:59 +0000

    Use conda base gcc9 image for developmetn

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ROOT="$( cd "$(dirname "$0")" ; pwd -P)"
 
-docker run -it --rm -v $ROOT:/opt/src julitopower/dockerclang9emacs25 /bin/bash
+docker run -it --rm -v $ROOT:/opt/src julitopower/dockercppdevemacsclang:gcc9 /bin/bash
