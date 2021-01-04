b03eff3fe22972ab8e9ae87ee8ce30582ada3f8e
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2021-01-04 06:40:56 -0800

    Set Workdir in Docker container creation

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$(dirname "$0")" ; pwd -P)"
+WD="/opt/src"
 
-docker run -it --rm -v $ROOT:/opt/src julitopower/dockercppdevemacsclang:gcc9 /bin/bash
+docker run -it --rm -v $ROOT:$WD -w $WD julitopower/dockercppdevemacsclang:gcc9 /bin/bash
