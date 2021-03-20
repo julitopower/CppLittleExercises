0c45f3ef5cfba81905a2cadb0e6d8fb3a04bcfe4
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2021-03-19 20:05:57 -0700

    Comment Command and CommandQueue

--- command/command.hpp
+++ command/command.hpp
@@ -1,3 +1,4 @@
+#include <bits/c++config.h>
 #include <functional>
 #include <list>
 #include <thread>
@@ -5,30 +6,59 @@
 
 using Fn = std::function<void()>;
 
+/*! \brief A Command encapuslates a deferred action
+ *
+ * For instance, on a web server one may perform
+ * certain actions synchronously with a request, but
+ * enqueu others, like logging information so that
+ * requests can be fullfilled fast, and non essential
+ * activities can be carried out asynchronously.
+ */
 class Command {
  public:
+  /*
+   * The Function passed must be self contained and if
+   * statefull, it must ensure it owns all its state, e.g.
+   * data structures capturved via lambda capture.
+   */
   Command(Fn fn) : fn_(fn) {}
 
-  void exec() {
-    fn_();
+
+  /* Commands are executed on a best effort fashion */
+  void exec() noexcept {
+    try {
+      fn_();
+    } catch (...) {}
   }
  private:
   Fn fn_;
 };
 
+/*! \brief Queue to communicate consumer and producers of Commands
+ */
 class CommandQueue {
  public:
 
-  static Command noop() {
+  /*! \brief Get a Command that does nothing*
+   *  Each call to this method returns the same command
+   *  object.
+   */
+  static const Command& noop() {
     static Command cmd([](){});
     return cmd;
   }
-  
+
+  /*! \brief Enqueue a command
+   *
+   */
   void add(Command cmd) {
     std::lock_guard<std::mutex> lock{mutex_};
     queue.push_front(cmd);
   }
 
+  /*! \brief Dequeue a command
+   *
+   */
   Command get() {
     std::lock_guard<std::mutex> lock{mutex_};
     if (!queue.empty()) {
@@ -38,6 +68,15 @@ class CommandQueue {
     }
     std::this_thread::yield();
     return CommandQueue::noop();
+  }
+
+  /*! \brief Elements left in the queue
+   *
+   *  This method is not protected by a mutex, and thus
+   *  the results may be inconsistent.
+   */
+  std::size_t size() const {
+    return queue.size();
   }
   
  private:
